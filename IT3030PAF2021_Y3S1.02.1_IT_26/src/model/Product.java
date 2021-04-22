794ce8f1555a66a596435af26b6aa142ad63c664
WeerasingheWSH <it19042220@my.sliit.lk>
2021-04-22 22:40:13 +0530

    After Creating the all the project specification

---
+++ IT3030PAF2021_Y3S1.02.1_IT_26/src/model/Product.java
@@ -0,0 +1,17 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Product { 
+	// A common method to connect to the DB
+	
+}
